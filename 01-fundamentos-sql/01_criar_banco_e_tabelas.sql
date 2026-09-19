6faa33d7226a59ea776f19f39acf78cf9d5a9884
jeffersonjesussant <jeffersonjesussantos45@gmail.com>
2026-09-18 23:35:24 -0300

    feat: adicionando tabela clientes

--- 01-fundamentos-sql/01_criar_banco_e_tabelas.sql
+++ 01-fundamentos-sql/01_criar_banco_e_tabelas.sql
@@ -1,18 +1,6 @@
-CREATE DATABASE IF NOT EXISTS livraria_db;
-USE livraria_db;
-
-
-CREATE TABLE IF NOT EXISTS livros (
+CREATE TABLE IF NOT EXISTS clientes (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    titulo VARCHAR(150) NOT NULL,
-    autor VARCHAR(100) NOT NULL,
-    preco DECIMAL(5, 2) NOT NULL,
-    data_cadastro DATE NOT NULL
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
-
-
-INSERT INTO livros (titulo, autor, preco, data_cadastro) VALUES
-('Use a Cabeça Java', 'Bert Bates', 112.50, '2026-01-15'),
-('A Arte da Estatistica', 'David Spiegelhalter', 39.90, '2026-02-10'),
-('Entendendo Estruturas de Dados', 'Marcello La Rocca', 73.00, '2026-03-01');
-
